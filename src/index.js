ced5fbc3235dced2ea94f5d9f12cfa5b63b5afed
nerdfiles <nerdfiles@gmail.com>
2021-06-28 22:39:27 -0500

    moar bs

---
+++ src/index.js
@@ -0,0 +1,3 @@
+const container = require('./infra/containers');
+
+container.start();
---
+++ src/infra/containers/index.js
@@ -0,0 +1,11 @@
+const container = require('awilix');
+
+const database = require('../databases');
+const logging = require('../logging');
+
+const c = container();
+
+module.exports = c({
+});
+
+// EOF
